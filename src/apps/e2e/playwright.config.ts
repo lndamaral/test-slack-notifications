441f1b1f0ffa38edbb3762da58c8770afce964ed
Leonardo Amaral <lnd.amaral@gmail.com>
2025-01-24 17:00:58 -0300

    debug

--- src/apps/e2e/playwright.config.ts
+++ src/apps/e2e/playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'dot' : [
     ['list'],
-    ['html', { open: 'never' }]
+    ['html']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
